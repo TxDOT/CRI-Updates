db159fd6eef56ad5e04850e0118a3a54969da590
dprosack <59172965+dprosack@users.noreply.github.com>
2022-06-16 13:19:42 -0500

    footer update text

--- src/components/Map/mapFooter.vue
+++ src/components/Map/mapFooter.vue
@@ -3,9 +3,9 @@
       <!-- <v-card elevation="0" class="black--text" id="footerCard"> -->
         <!-- <v-card-text justify="center" v-if="isNaN(countyTots)&&isNaN(modifyLine)&&isNaN(modifyLength) ? 0: countyTots"> -->
           <div  id="footerCard" class="f1-text">County: <b>{{county}}</b>&nbsp; &nbsp;&nbsp; User Name: <b>Tester_TXDOT</b> 
-            &nbsp; &nbsp; &nbsp;Previous Total Mileage: <b>{{countyTotal}}</b>&nbsp;&nbsp;&nbsp; 
-            Current Mileage: <b>{{Number(rdbdDeltaDist.toFixed(4))}}</b>&nbsp;&nbsp;&nbsp; 
-            New Total Miles: <b>{{Number(countyTots.toFixed(4))}}</b>
+            &nbsp; &nbsp; &nbsp;Starting Mileage: <b>{{countyTotal}}</b>&nbsp;&nbsp;&nbsp; 
+            Mileage Change: <b>{{Number(rdbdDeltaDist.toFixed(4))}}</b>&nbsp;&nbsp;&nbsp; 
+            Updated Mileage: <b>{{Number(countyTots.toFixed(4))}}</b>
           </div>
           <div style="position: absolute; font-size: 10px; bottom: 10%;">
             Build v1.3
