4afc6c9e69640fcd184e19480c9412a3b428a66d
dprosack <59172965+dprosack@users.noreply.github.com>
2022-08-19 09:23:55 -0500

    update build number

--- src/components/Map/aboutApp.vue
+++ src/components/Map/aboutApp.vue
@@ -2,7 +2,7 @@
     <!-- <v-dialog style="position:absolute; left: 50%; top: 50%;"> -->
       <v-card id="finalCheck">
         <v-card-title class="surfaceTitle"><v-card-text style="position:relative; bottom:25px; text-align: left; padding-left: 0px;">About the County Road Inventory Map</v-card-text></v-card-title>
-        <v-card-text style="position: absolute; color: black; top: 0rem; left: 0%; font-size: 10px;"><u>Build Version: 1.4.4</u></v-card-text>
+        <v-card-text style="position: absolute; color: black; top: 0rem; left: 0%; font-size: 10px;"><u>Build Version: 1.4.5</u></v-card-text>
         <div style="width:100%;height:0;padding-bottom:100%;position:relative; right: 35%;"><iframe src="https://giphy.com/embed/fveEm9uqUas7igLGTU" width="70%" height="70%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
         <!-- <v-card-text style="poistion: absolute; text-align: left; color: black;">Overview: this is a new version of the DUSA application. Refer to this section to review updates to the map.</v-card-text> -->
         <v-card-text style="poistion: absolute; text-align: left; color: black;" v-for="(i, index) in updateList" :key=index><ul style="line-height: .5rem"><li>{{updateList[index]}}</li></ul></v-card-text>
