d07fb923a58555029725641af65835cc4770d7c5
dprosack <59172965+dprosack@users.noreply.github.com>
2022-08-24 10:15:49 -0500

    update build v1.4.6

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/apps/statewide_mapping/dusa/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/CRI-Updates/" : "/",
 };
