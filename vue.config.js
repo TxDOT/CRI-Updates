a1fa86f935790ad6d76e255701000b87a4e72b4b
mwashbur <35734485+mwashbur@users.noreply.github.com>
2022-09-01 11:20:04 -0500

    test comments

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   //publicPath: process.env.NODE_ENV === "production" ? "/CRI-Updates/" : "/",
+  //control url here
   publicPath: process.env.NODE_ENV === "production" ? "/apps/statewide_mapping/cri/" : "/",
 };
