55167fec7bcfc4b24ca3fbb5a58b643d58a4d43f
dprosack <59172965+dprosack@users.noreply.github.com>
2022-10-27 16:47:41 -0500

    1.5.4 build - test env

--- src/components/Map/aboutApp.vue
+++ src/components/Map/aboutApp.vue
@@ -2,25 +2,30 @@
     <!-- <v-dialog style="position:absolute; left: 50%; top: 50%;"> -->
     <v-dialog v-model="aboutClick" max-width="500" persistent style="overflow-y: hidden;">
         <v-card id="finalCheck" v-model="aboutClick" min-height="540">
-        <v-card-title class="surfaceTitle"><span style="position: relative; bottom: 10px; right: 7px;">About the County Road Inventory Map</span></v-card-title>
-        <v-card-text style="position: relative; color: black; top: 28.5rem; left: 0%; font-size: 10px; text-align: left;"><u>Build Version: 1.5.3</u></v-card-text>
-        <div>
-            <v-card-text style="position: relative; color: black; bottom:2.5rem; left: 0%; text-align: left;">The County Road Inventory Map is a tool for county officials to verify and make updates to TxDOT’s 
+            <v-card-title class="surfaceTitle"><span style="position: relative; bottom: 10px; right: 7px;">About the County Road Inventory Map</span></v-card-title>
+            
+            <div>
+                <v-card-text style="position: relative; color: black; bottom:0.5rem; left: 0%; text-align: left;">The County Road Inventory Map is a tool for county officials to verify and make updates to TxDOT’s 
                                                                                                         roadway inventory. <a id="hoverLink" href='https://capitol.texas.gov/tlodocs/74R/billtext/html/SB00971F.htm' target="_blank">
                                                                                                         The Texas Legislature requires TxDOT to administer this program</a> to ensure accuracy
                                                                                                         of the mileage reported to the Department of Motor Vehicles and Comptroller of Public Accounts for the
                                                                                                         purpose of allocation of highway funds.</v-card-text>
-        </div>
-        <v-divider style="position: relative; bottom: 1.5rem; color:black;"></v-divider>
-        <!-- <v-card-text style="poistion: absolute; text-align: left; color: black;">Overview: this is a new version of the DUSA application. Refer to this section to review updates to the map.</v-card-text> -->
+            </div>
+            <v-divider style="position: relative; bottom: .5rem; color:black;"></v-divider>
+            <!-- <v-card-text style="poistion: absolute; text-align: left; color: black;">Overview: this is a new version of the DUSA application. Refer to this section to review updates to the map.</v-card-text> -->
 
-        <div style="position:inherit; top: 20rem;">
-            <v-card-text style="text-align: left; color: black; margin-left: 1rem;" v-for="(i, index) in updateList" :key=index><ul style="position:relative; line-height: 1px; white-space: nowrap; padding: 0rem;"><li>{{updateList[index]}}</li></ul></v-card-text>
-        </div>
+            <div style="position:relative; top: .5rem;">
+                <v-card-text style="text-align: left; color: black; margin-left: 1rem;" v-for="(i, index) in updateList" :key=index><ul style="position:relative; line-height: 1px; white-space: nowrap; padding: 0rem;"><li>{{updateList[index]}}</li></ul></v-card-text>
+            </div>
 
-        <div style="position: relative; left: 12rem; top: 4rem;"><v-btn style="border:1px solid black;" underline tile color="#14375A" outlined @click="close(); clearEditBtn=false"><u>close</u></v-btn></div>
+            <div style="position: relative; left: 4rem; top: 2rem; padding-bottom: .5rem;">
+                <v-card-text style="position:absolute; color: black; bottom: 0rem; right: 3.6rem; font-size: 10px; text-align: left;"><u>Build Version: 1.5.4</u></v-card-text>
+                <v-btn style="position: relative; bottom: 0rem; left: 8rem; border:1px solid black;" underline tile color="#14375A" outlined @click="close(); clearEditBtn=false">
+                    <u>close</u>
+                </v-btn>
+            </div>
         
-      </v-card>
+        </v-card>
     </v-dialog>
     <!-- </v-dialog> -->
 </template>
@@ -30,7 +35,7 @@ export default {
     name: 'about',
     data (){
       return {
-        updateList:['Added Geometry Checks (Self-Intersection, Minimum geometry length)', 'Updated sign-up URL','Added link to training videos','Adjusted Action item Body text font', 'Disabled mile info after ending asset mileage is completed', 
+        updateList:['Added Upload/Download Inventory','Added Download Road Log','Added Geometry Checks (Self-Intersection, Minimum geometry length)', 'Updated sign-up URL','Added link to training videos','Adjusted Action item Body text font', 'Disabled mile info after ending asset mileage is completed', 
                     'Adjusted Missing Attributes/Discard Edit Popup']
       }
     },
