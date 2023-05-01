c281f3a79c85411f255719a74d673f61b9134309
dprosack <59172965+dprosack@users.noreply.github.com>
2023-05-01 14:00:56 -0500

    new build

--- src/common/cri_constants.js
+++ src/common/cri_constants.js
@@ -9,7 +9,7 @@ export const criConstants = {
     advanceUploadLayer: "https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/CRI_QAQC_prod/FeatureServer/0",//'https://services.arcgis.com/KTcxiTD9dsQw4r7Z/ArcGIS/rest/services/CRI_QAQC_dev/FeatureServer/0',
     txCounties: "https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/Texas_County_Boundaries/FeatureServer/0",
     judgeInfoTable: "https://services.arcgis.com/KTcxiTD9dsQw4r7Z/ArcGIS/rest/services/CNTY_INFO_dev/FeatureServer/0",
-    deleteDocumentFL: "https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/CRI_DELETE_DOCUMENTATION_DEV/FeatureServer/0",
+    deleteDocumentFL: "https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/CRI_Delete_Documentation_prod/FeatureServer/0",//"https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/CRI_DELETE_DOCUMENTATION_DEV/FeatureServer/0",
 
     webhookUrl: "https://testportal.txdot.gov/fmejobsubmitter/TPP/TPP_DEV_DUSA2Email.fmw?params=",
     headerName: "Authorization",
