71284c7f835c67a32bb3b8d9501b7227af917961
dprosack <59172965+dprosack@users.noreply.github.com>
2023-06-06 09:05:36 -0500

    changed webhooks to from prod to dnd

--- src/components/Map/advanced.js
+++ src/components/Map/advanced.js
@@ -382,7 +382,7 @@ function serverResponse(submitid){
     store.commit('setIsFmeProcess', [true, ""])
     // --Current -- let dataReturn = await fetch(`https://gis-batch-dnd.txdot.gov/fmejobsubmitter/TPP-MB/CRI_QAQC_dev.fmw?SUBMIT_ID=${submitid}&EMAIL=${store.getters.getUserEmail}&USERNAME=${store.getters.getUserName}&opt_showresult=false&opt_servicemode=sync`, {headers:{'Authorization':'fmetoken token=ef92b878734df046a715c1e39d46cb40f1f321fd', 'Content-Type': 'text/plain', 'Access-Control-Allow-Private-Network': true}})
     //https://gis-batch-dev.txdot.gov/fmejobsubmitter/TPP/CRI_QAQC_dev_CORS.fmw?SUBMIT_ID=${submitid}&USERNAME=${store.getters.getUserName}&EMAIL=${store.getters.getUserEmail}&opt_showresult=false&opt_servicemode=sync
-    let dataReturn = fetch(`https://testportal.txdot.gov/fmejobsubmitter/TPP/TPP_DEV_CRI_QAQC.fmw?SUBMIT_ID=${submitid}&USERNAME=${store.getters.getUserName}&EMAIL=${store.getters.getUserEmail}&opt_showresult=false&opt_servicemode=sync`, {headers:{'Authorization':'fmetoken token=f1cdc75a1b3ddd13167fb0e4cb6e6301b0373889', 'Content-Type': 'text/plain'}})
+    let dataReturn = fetch(`https://gis-batch-dnd.txdot.gov/fmejobsubmitter/TPP-MB/CRI_QAQC_dev.fmw?SUBMIT_ID=${submitid}&USERNAME=${store.getters.getUserName}&EMAIL=${store.getters.getUserEmail}&opt_showresult=false&opt_servicemode=sync`, {headers:{'Authorization':'fmetoken token=344def2a86f0bf40427ea75e868c84287035e3c0', 'Content-Type': 'text/plain'}})
     dataReturn
         .then(() =>{
             store.commit('setIsFmeProcess', [false, "success"])
