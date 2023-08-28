4a832240328c8ead658d1b2160eeb58368ccb2f0
dprosack <59172965+dprosack@users.noreply.github.com>
2023-08-28 12:37:12 -0500

    added eoy reminder

---
+++ src/components/Map/reminder.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-alert type="warning" border="left" color="#c55a11" id="eoeWarning" tile dismissible>
+        <b>IMPORTANT:</b> <u>Submit and Certify</u> edits by the August 31 deadline.
+    </v-alert>
+</template>
+<script>
+export default {
+    name: "eoeWarning",
+    data() {
+        return{
+
+        }
+    },
+
+}
+</script>
+<style scoped>
+    #eoeWarning{
+        position: relative;
+        width: 30vw;
+        left: 39vw;
+        bottom:.8rem;
+    }
+</style>
