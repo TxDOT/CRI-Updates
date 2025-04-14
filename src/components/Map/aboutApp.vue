de427e982d463899348d9d9c018962fff800485b
Sal <salvador.saldana@txdot.gov>
2025-04-14 13:07:23 -0500

    updates release notes

--- src/components/Map/aboutApp.vue
+++ src/components/Map/aboutApp.vue
@@ -18,7 +18,7 @@
 
         <div id="btnPosition"><v-btn underline tile color="#0056a9" outlined @click="close(); clearEditBtn=false"><u>close</u></v-btn></div>
 
-        <v-card-text class="textSymb" id="buildNm"><u>Build Version: 1.6.13</u></v-card-text>
+        <v-card-text class="textSymb" id="buildNm"><u>Build Version: 1.7.0</u></v-card-text>
 
             
       </v-card>
@@ -30,8 +30,7 @@ export default {
     name: 'about',
     data (){
       return {
-        updateList:["Added layer showing Last Year's Edits", "Removed Design/Surface/Lane attributes", "Added self-guided tour popups", "Added short training videos (Add Road, Edit Road, Delete Road, Save & Exit, Submit & Certify, Overview)", "Enhanced registration process",
-                    "Integrated aerial imagery basemap with editing tools", "Added End of Year cutoff process", "Starting mileage update popup info box"]
+        updateList:["Added chat feature", "Updated editing tools", "Updated short training videos", "Updated map colors to new TxDOT blue"]
       }
     },
     methods:{
