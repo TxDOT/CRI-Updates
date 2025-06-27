1b65d2fabf26ab87ca1e451e119b5b9796792f27
dprosack <david.prosack@txdot.gov>
2025-06-27 16:33:39 -0500

    github build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //publicPath: process.env.NODE_ENV === "production" ? "/CRI-Updates/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/CRI-Updates/" : "/",
   //control url here
-  publicPath: process.env.NODE_ENV === "production" ? "/apps/statewide_mapping/cri/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/apps/statewide_mapping/cri/" : "/",
 };
