9ef0a2219a6c975c4de346d19933c6df8f0ffab4
Sal <salvador.saldana@txdot.gov>
2026-03-26 10:55:06 -0500

    changed the upload data webhook with webhook create by sal

--- src/components/Map/advanced.js
+++ src/components/Map/advanced.js
@@ -300,7 +300,11 @@ function serverResponse(submitid){
     store.commit('setIsFmeProcess', [true, ""])
     // --Current -- let dataReturn = await fetch(`https://gis-batch-dnd.txdot.gov/fmejobsubmitter/TPP-MB/CRI_QAQC_dev.fmw?SUBMIT_ID=${submitid}&EMAIL=${store.getters.getUserEmail}&USERNAME=${store.getters.getUserName}&opt_showresult=false&opt_servicemode=sync`, {headers:{'Authorization':'fmetoken token=ef92b878734df046a715c1e39d46cb40f1f321fd', 'Content-Type': 'text/plain', 'Access-Control-Allow-Private-Network': true}})
     //https://gis-batch-dev.txdot.gov/fmejobsubmitter/TPP/CRI_QAQC_dev_CORS.fmw?SUBMIT_ID=${submitid}&USERNAME=${store.getters.getUserName}&EMAIL=${store.getters.getUserEmail}&opt_showresult=false&opt_servicemode=sync
-    let dataReturn = fetch(`https://testportal.txdot.gov/fmejobsubmitter/TPP/TPP_DEV_CRI_QAQC_AssetsRemove.fmw?SUBMIT_ID=${submitid}&USERNAME=${store.getters.getUserName}&EMAIL=${store.getters.getUserEmail}&opt_showresult=false&opt_servicemode=sync`, {headers:{'Authorization':'fmetoken token=8a1e669e82e32009a2fa81e757261b1dee1aa597', 'Content-Type': 'text/plain'}})
+    let dataReturn = fetch(`https://testportal.txdot.gov/fmejobsubmitter/TPP/TPP_DEV_CRI_QAQC_AssetsRemoveDEV.fmw?SUBMIT_ID=${submitid}&USERNAME=${store.getters.getUserName}&EMAIL=${store.getters.getUserEmail}&opt_showresult=false&opt_servicemode=sync`, {headers:{'Authorization':'fmetoken token=a264041e806b7199db0384c0357c2bc08b882750', 'Content-Type': 'text/plain'}})
+    
+    
+    
+    
     dataReturn
         .then(() =>{
             store.commit('setIsFmeProcess', [false, "success"])
